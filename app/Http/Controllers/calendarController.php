5faec094ddd68f5e84a7b99f277435538d3cdf0b
kyooo2001 <kyooo2001@gmail.com>
2024-12-28 18:57:08 -0500

    Set views: php artisan make:controller nombreController -r -m newNombre by Felipe

---
+++ app/Http/Controllers/calendarController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\newCallendar;
+use Illuminate\Http\Request;
+
+class calendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(newCallendar $newCallendar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(newCallendar $newCallendar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, newCallendar $newCallendar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(newCallendar $newCallendar)
+    {
+        //
+    }
+}
